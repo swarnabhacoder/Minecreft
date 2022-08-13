b8a70267e8411c578605a15fb00144d6f98990b4
swarnabhacoder <103578241+swarnabhacoder@users.noreply.github.com>
2022-08-13 11:43:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,149 @@
+// Create the reference variable of the canvas using fabric.Canvas()
+var canvas=new fabric.Canvas('myCanvas');
+
+
+// Define the starting width and height of the block images
+block_image_width=30;
+block_image_height=30;
+
+
+
+// Define the starting x and y coordinates for the player
+player_x=10;
+player_y=10;
+
+
+// Define a variable named player_object to store object of the player image
+var player_object="";
+
+
+// Add a function named player_update() to add the player image
+function player_update(){
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({top:player_y,left:player_x});
+        canvas.add(player_object);
+    });
+}
+function new_image(get_image){
+    fabric.Image.fromURL(get_image,function(Img){
+        block_image_object=Img;
+        block_image_object.scaleToWidth(block_image_width);
+        block_image_object.scaleToHeight(block_image_height);
+        block_image_object.set({top:player_y,left:player_x});
+
+        canvas.add(
+            block_image_object
+        );
+    });
+
+}
+
+
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    var keyPressed=e.keyCode;
+    console.log(keyPressed);
+    if(e.shiftKey==true&&keyPressed=='80')
+    {
+        block_image_height=block_image_height+10;
+        block_image_width=block_image_width+10;
+        document.getElementById("current_height").innerHTML=block_image_height;
+        document.getElementById("current_width").innerHTML=block_image_width;
+    }
+    if(e.shiftKey==true&&keyPressed=='77')
+    {
+        block_image_height=block_image_height-10;
+        block_image_width=block_image_width-10;
+        document.getElementById("current_height").innerHTML=block_image_height;
+        document.getElementById("current_width").innerHTML=block_image_width;
+    }
+    if(keyPressed=='67'){
+        new_image('cloud.jpg');
+    }
+    if(keyPressed=='68'){
+        new_image('dark_green.png');
+    }
+    if(keyPressed=='71'){
+        new_image('ground.png');
+    }
+    if(keyPressed=='76'){
+        new_image('light_green.png');
+    }
+    if(keyPressed=='82'){
+        new_image('roof.jpg');
+    }
+    if(keyPressed=='84'){
+        new_image('trunk.jpg');
+    }
+    if(keyPressed=='85'){
+        new_image('unique.png');
+    }
+    if(keyPressed=='87'){
+        new_image('wall.jpg');
+    }
+    if(keyPressed=='89'){
+        new_image('yellow_wall.png');
+    }
+    if(keyPressed=='38'){
+        up();
+    }
+    if(keyPressed=='37'){
+        left();
+    }
+    if(keyPressed=='39'){
+        right();
+    }
+    if(keyPressed=='40'){
+        down();
+    }
+}
+
+function up(){
+    
+     if(player_y>=0){
+        player_y=player_y-block_image_width;
+    canvas.remove(player_object);
+    player_update()
+     }   
+}
+
+
+function down(){
+   
+     if(player_y<=550){
+        player_y=player_y+block_image_height;
+        canvas.remove(player_object);
+        player_update()
+     }   
+}
+
+
+function left(){
+   
+    if(player_x>=0)
+    player_x=player_x-block_image_width;
+    canvas.remove(player_object);
+    player_update()   
+}
+
+
+function right(){
+   
+     if(player_x<=850) {
+        player_x=player_x+block_image_height;
+        canvas.remove(player_object);
+        player_update()
+     }  
+}
+
+
+
+
+
+
+
+// Add a function named new_image() to add the different images as per the specific keys pressed
+
